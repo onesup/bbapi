1c5e9d21e4978de0f2841a4f1a5a0adb2af43494
Jaehue Jang <bbugguj@Jaehue-ui-MacBook-Pro.local>
2013-10-06 07:44:22 +0900

    comment 모델에 wirter_id 필수 조건 추가

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :writer, class_name: 'User'
   belongs_to :commentable, polymorphic: true
+
+  # 유효성 검증 => writer_id 는 필수항목임
+  validates_presence_of :writer_id  
 end
