6d67e5b920b5312aea26fe641e6002a57c5aa894
Jaehue Jang <bbugguj@Jaehue-ui-MacBook-Pro.local>
2013-10-06 09:22:55 +0900

    comment 유효성 검증(writer_id, commentable_id) 테스트 코드 추가

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :writer, class_name: 'User'
   belongs_to :commentable, polymorphic: true
 
-  # 유효성 검증 => writer_id 는 필수항목임
-  validates_presence_of :writer_id  
+  # 유효성 검증 => writer_id, commentable_id 는 필수항목임
+  validates_presence_of :writer_id , :commentable_id
 end
