e117cc001fd713c5f81f3e4d7a1bee73542de295
chungsub.kim <chungsub.kim@purpleworks.co.kr>
2013-10-09 20:44:25 +0900

    use token_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::API
   include ActionController::StrongParameters
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
-
+  before_filter :authenticate_user_from_token!
+  
   protected
 
   def configure_permitted_parameters
@@ -11,4 +12,11 @@ class ApplicationController < ActionController::API
     devise_parameter_sanitizer.for(:sign_in) 
   end
 
+  def authenticate_user_from_token!
+    user_email = request.headers["X-Auth-Email"].presence
+    user       = user_email && User.find_by_email(user_email)
+    if user && Devise.secure_compare(user.authentication_token, request.headers["X-Auth-Token"])
+      sign_in user, store: false
+    end
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
-  before_filter :authenticate_user!, :except => [:create, :destroy]
+  before_filter :authenticate_user!, :except => [:create]
   respond_to :json
 
   def create
@@ -8,20 +8,22 @@ class Users::SessionsController < Devise::SessionsController
 
     if resource.valid_password?(params[:password])
       sign_in(:user, resource)
-      # resource.ensure_authentication_token!
+      resource.ensure_authentication_token!
       render :json=> {:success=>true, :auth_token=>resource.authentication_token, :email=>resource.email, :id=>resource.id}
       return
     end
+
     invalid_login_attempt
   end
 
   def destroy
-    # resource = User.find_for_database_authentication(:email => params[:email])
-    # resource.authentication_token = nil
-    # resource.save
+    current_user.authentication_token = nil;
+    current_user.save
     sign_out :user
+
     render :json=> {:success=>true}
   end
+
 
   protected
 
