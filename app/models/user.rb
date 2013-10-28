f6620feb74e52a3c936d8151e6bf710aad2a48ce
subicura <subicura@subicura.com>
2013-10-28 15:01:57 +0900

    add paperclip url setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   has_many :authorizations
 
   has_attached_file :avatar, 
-                    :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+                    :styles => { :medium => "300x300#", :thumb => "100x100#" }, 
                     :default_url => "/images/default/:style_avatar.png",
                     :storage => :s3,
                     :s3_credentials => "#{Rails.root}/config/s3.yml",
