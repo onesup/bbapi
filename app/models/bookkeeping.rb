a4ca960b29060cfcf3dac54edc79f6c9cc4015e3
Jaehue Jang <bbugguj@Jaehue-ui-MacBook-Pro.local>
2013-11-08 11:40:41 +0900

    Bookkeeping 모델에 likers 추가. json serialze시 liker_ids 포함시킴

--- app/models/bookkeeping.rb
+++ app/models/bookkeeping.rb
@@ -5,6 +5,8 @@ class Bookkeeping < ActiveRecord::Base
   belongs_to :group
   has_many :proofs, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :likes, as: :likeable, dependent: :destroy
+  has_many :likers, class_name: 'User', foreign_key: :user_id, through: :likes,  source: :liker  
 
   validates :group_id, presence: true
   validates :account_title_id, presence: true
--- app/serializers/bookkeeping_serializer.rb
+++ app/serializers/bookkeeping_serializer.rb
@@ -1,8 +1,9 @@
 class BookkeepingSerializer < ActiveModel::Serializer
-  attributes :id, :issue_date, :operator, :remark, :amount, :content, :created_at, :updated_at
+  attributes :id, :issue_date, :operator, :remark, :amount, :content, :likes_count, :created_at, :updated_at
   has_one :issuer
   has_one :writer
   has_one :account_title
   has_many :proofs
   has_many :comments
+  has_many :likers, embed: :ids
 end
